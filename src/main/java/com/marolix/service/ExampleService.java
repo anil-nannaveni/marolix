b8e453ad8be92c7b7af8adda45d8765104b86676
anil-nannaveni <128711890+anil-nannaveni@users.noreply.github.com>
2023-05-10 16:13:33 +0530

    Update ExampleService.java

--- src/main/java/com/marolix/service/ExampleService.java
+++ src/main/java/com/marolix/service/ExampleService.java
@@ -9,7 +9,7 @@ public class ExampleService {
 
     @GET
     public Response get() {
-        return Response.ok("Hello, worldprogramme!").build();
+        return Response.ok("Hello, worldprogramme1!").build();
     }
 
 }
